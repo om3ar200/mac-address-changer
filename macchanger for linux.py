47bb679bce6d2d5e9d49103dea7768f7d2378bb2
Omar Ahmed <Omar.Ahmed.2401538@science.helwan.edu.eg>
2026-06-27 21:20:26 +0300

    Add files via upload

---
+++ macchanger for linux.py
@@ -0,0 +1,14 @@
+import subprocess
+import random
+def generate_random_mac():
+    mac = [0x00, 0x16, 0x3e,
+           random.randint(0x00, 0x7f),
+           random.randint(0x00, 0xff),
+           random.randint(0x00, 0xff)]
+    return ':'.join(map(lambda x: "%02x" % x, mac))
+new_mac = generate_random_mac()
+interface = "eth0"
+subprocess.call(f"ifconfig {interface} down", shell=True)
+subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
+subprocess.call(f"ifconfig {interface} up", shell=True)
+print(f"the new MAC address is: {new_mac}")
